d99285810715b7cb0590c072cb32d89903bce2c4
John Williams <jrw+3@mit.edu>
2023-11-30 16:54:39 +0000

    image added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # reactionTest - author jrw@mit.edu
 
 ## test users reaction time to click on button
+## image is served up from static  /public directory
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let data = [99999];
-
-let root = path.resolve(__dirname) + '/public/';
+app.use(express.static(__dirname + '/public'));
+let root = path.resolve(__dirname, 'public');
 app.get('/', (req, res) => {
     res.sendFile('block.html', { 'root': root });
 });
@@ -29,5 +29,5 @@ app.get('/pac', function(req, res) {
     res.sendFile('/PacMan' + req.query.id + '.png', { 'root': root });
 });
 
-app.listen(3000);
-console.log("Running on port 3000");
+app.listen(4000);
+console.log("Running on port 4000");
